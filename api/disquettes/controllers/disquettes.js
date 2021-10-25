c63aa73029d6c9dd60d76c6f86220634219ba2f9
LazareC <lazarela17@gmail.com>
2021-10-25 15:04:27 +0200

    add tags and many update

--- api/disquettes/controllers/disquettes.js
+++ api/disquettes/controllers/disquettes.js
@@ -3,8 +3,19 @@ const { sanitizeEntity, parseMultipartData } = require('strapi-utils');
 module.exports = {
 
     async random (ctx) {
-
         const randomDisquette = await strapi.services.disquettes.random();
         return sanitizeEntity(randomDisquette, { model: strapi.models.disquettes });
-    }
+    },
+    async create(ctx) {
+        let entity;
+        if (ctx.is('multipart')) {
+          const { data, files } = parseMultipartData(ctx);
+          data.creator = ctx.state.user.id;
+          entity = await strapi.services.disquettes.create(data, { files });
+        } else {
+          ctx.request.body.creator = ctx.state.user.id;
+          entity = await strapi.services.disquettes.create(ctx.request.body);
+        }
+        return sanitizeEntity(entity, { model: strapi.models.disquettes });
+      },
 };
